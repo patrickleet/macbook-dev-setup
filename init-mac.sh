f5b1b950837154f651ef71f82fd6183925a85c85
Patrick Scott <pat@patscott.io>
2017-04-09 19:36:01 -0400

    configure git and create dev directory

--- init-mac.sh
+++ init-mac.sh
@@ -6,3 +6,17 @@ brew install git
 
 # install ansible
 brew install ansible
+
+# setup git
+echo "Github requires a few global settings to be configured"
+echo "Enter the email address associated with your GitHub account: "
+read -r email
+echo "Enter your full name (Ex. John Doe): "
+read -r username
+
+git config --global --replace-all user.email "$email"
+git config --global --replace-all user.name "$username"
+
+# setup dev directory
+mkdir ~/dev/
+cd ~/dev/
