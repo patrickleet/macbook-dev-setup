db869ff9e4a8f632782b867d170125b7bca32137
Patrick Scott <pat@patscott.io>
2017-04-09 21:51:59 -0400

    Update init-mac.sh

--- init-mac.sh
+++ init-mac.sh
@@ -53,7 +53,7 @@ n use latest
 mkdir ~/dev/
 
 # set up vscode
-code --install-extension PeterJausovec.vscode-docker --install-extension dbaeumer.vscode-eslint --install-extension dzannotti.vscode-babel-coloring --install-extension ilich8086.classic-asp --install-extension marcostazi.VS-code-vagrantfile --install-extension mauve.terraform --install-extension ms-vscode.PowerShell --install-extension neilding.language-liquid --install-extension rtorr.vscode-flow --install-extension secanis.jenkinsfile-support
+code --install-extension PeterJausovec.vscode-docker --install-extension dbaeumer.vscode-eslint --install-extension dzannotti.vscode-babel-coloring --install-extension ilich8086.classic-asp --install-extension marcostazi.VS-code-vagrantfile --install-extension mauve.terraform --install-extension ms-vscode.PowerShell --install-extension neilding.language-liquid --install-extension secanis.jenkinsfile-support
 
 pbcopy < ~/.ssh/id_rsa.pub
 echo "Add the generated SSH key to your GitHub account. It has been copied to your clipboard"
