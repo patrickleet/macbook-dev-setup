50690fab22e1e088a8c53873b2f7bef8ad6efe44
Patrick Scott <pat@patscott.io>
2017-04-09 20:51:24 -0400

    add Spotify

--- init-mac.sh
+++ init-mac.sh
@@ -28,6 +28,7 @@ brew cask install kitematic
 brew cask install hyper
 brew cask install visual-studio-code
 brew cask install slack
+brew cask install spotify
 brew install terraform
 brew install packer
 brew install node
