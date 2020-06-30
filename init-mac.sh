14a77f2320909cd6cf17ef834883be3c2bdc956b
Patrick Lee Scott <pat@patscott.io>
2020-06-30 16:04:02 -0400

    feat: updates

--- init-mac.sh
+++ init-mac.sh
@@ -44,14 +44,16 @@ git config --global --replace-all user.email "$email"
 git config --global --replace-all user.name "$username"
 
 # SSH Key
-echo "Generating an SSH Key - this will be added to your agent for you"
-ssh-keygen -t rsa -b 4096 -C "$email"
-echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_rsa" | tee ~/.ssh/config
-eval "$(ssh-agent -s)"
+if [ -f "~/.ssh/id_rsa" ]; then
+    echo "SSH Key exists"
+else
+    echo "Generating an SSH Key - this will be added to your agent for you"
+    ssh-keygen -t rsa -b 4096 -C "$email"
+    echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_rsa" | tee ~/.ssh/config
+    eval "$(ssh-agent -s)"
+fi
 
-#  Configure ZSH
-echo "Configuring ZSH"
-sleep 1
+echo "Configuring Oh My ZSH"
 { # your 'try' block
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 } || { # your 'catch' block
@@ -60,24 +62,18 @@ sleep 1
 
 # Configure ZSH  plugins
 echo "Configuring ZSH plugins"
-sleep 1
 {
     git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-    echo "plugins+=(zsh-autosuggestions)" >> ~/.zshrc
-
     git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-    echo "plugins+=(zsh-syntax-highlighting)" >> ~/.zshrc
-
-    git clone git@github.com:Dbz/kube-aliases.git ~/.oh-my-zsh/custom/plugins/kube-aliases
-    echo "plugins+=(kube-aliases)" >> ~/.zshrc
-
+    sed -i 's/plugins=(git)/plugins=(\n  git\n)/g' ~/.zshrc
+    sed -i 's/git$/git\'$'\n  zsh-autosuggestions/g' ~/.zshrc
+    sed -i 's/zsh-autosuggestions$/zsh-autosuggestions\'$'\n  zsh-syntax-highlighting/g' ~/.zshrc
 } || {
     echo 'Failed to configure zsh plugins'
 }
 
 # caskroom https://caskroom.github.io
 echo "Installing Brew apps"
-sleep 1
 {
     brew tap caskroom/cask
     brew tap jenkins-x/jx
@@ -97,9 +93,7 @@ sleep 1
     brew install node
     brew install python
     brew install jq
-    brew install kubernetes-cli
     brew install kops
-    brew install jx
     brew install kafka
     brew install tmux
     brew install tree
@@ -108,16 +102,61 @@ sleep 1
     echo "One or more brew formulas failed to install"
 }
 
+# JX
+if [ -x "$(command -v jx)" ]; then
+    echo "jx installed"
+else
+    brew install jx
+    echo "\nsource <(jx completion zsh)" >> ~/.zshrc
+    echo "\nexport PATH=$HOME/.jx/bin:$PATH" >> ~/.zshrc
+fi;
+
+# kubectl
+if [ -x "$(command -v kubectl)" ]; then
+    echo "kubectl installed"
+else
+    brew install kubernetes-cli
+    echo "\nsource <(kubectl completion zsh)" >> ~/.zshrc
+fi;
+
+if [ -x "$(command -v sops)" ]; then
+    echo "sops installed"
+else
+    brew install sops
+fi;
+
+# safe
+if [ -x "$(command -v safe)" ]; then
+    echo "safe installed"
+else
+    brew tap starkandwayne/cf
+    brew install starkandwayne/cf/safe
+fi;
+
 # Make sure user is owner of homebrew directory
 echo "For n to work properly, you need to own homebrew stuff - setting $(whoami) as owner of $(brew --prefix)/*"
 sudo chown -R $(whoami) $(brew --prefix)/*
 
-echo "Installing n"
-npm install -g n
+# N
+if [ -x "$(command -v n)" ]; then
+    echo "N - Node version manager installed with latest LTS of Node"
+else
+    npm install -g n
+    sudo mkdir -p /usr/local/n
+    sudo chown -R $(whoami) /usr/local/n
+    # take ownership of node install destination folders
+    sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+    # Use the latest version of Node
+    echo "Using latest version of Node"
+    n latest
+fi;
 
-# Use the latest version of Node
-echo "Using latest version of Node"
-n latest
+if [ -x "$(command -v aws)" ]; then
+    echo "aws cli installed"
+else
+    curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+    sudo installer -pkg AWSCLIV2.pkg -target /
+fi;
 
 echo "Installing hyper term package manager and meta"
 npm i -g hpm-cli meta
@@ -129,30 +168,25 @@ mkdir -p ~/Documents/dev
 # set up vscode
 echo "Configure VS Code extensions"
 sleep 1
-code --install-extension PeterJausovec.vscode-docker \
-     --install-extension marcostazi.VS-code-vagrantfile \
-     --install-extension mauve.terraform \
-     --install-extension secanis.jenkinsfile-support \
-     --install-extension formulahendry.code-runner \
-     --install-extension mikestead.dotenv \
-     --install-extension oderwat.indent-rainbow \
-     --install-extension orta.vscode-jest \
-     --install-extension jenkinsxio.vscode-jx-tools \
-     --install-extension mathiasfrohlich.kotlin \
-     --install-extension christian-kohler.npm-intellisense \
-     --install-extension sujan.code-blue \
-     --install-extension waderyan.gitblame \
-     --install-extension ms-vscode.go \
-     --install-extension in4margaret.compareit \
-     --install-extension andys8.jest-snippets \
-     --install-extension euskadi31.json-pretty-printer \
-     --install-extension yatki.vscode-surround \
-     --install-extension wmaurer.change-case
-
-# awscli and invoke - kinda like make for python
-echo "installing awscli"
-sleep 1
-pip3 install awscli --upgrade --user
+code    --install-extension ms-azuretools.vscode-docker \
+        --install-extension marcostazi.VS-code-vagrantfile \
+        --install-extension mauve.terraform \
+        --install-extension secanis.jenkinsfile-support \
+        --install-extension formulahendry.code-runner \
+        --install-extension mikestead.dotenv \
+        --install-extension oderwat.indent-rainbow \
+        --install-extension orta.vscode-jest \
+        --install-extension jenkinsxio.vscode-jx-tools \
+        --install-extension mathiasfrohlich.kotlin \
+        --install-extension christian-kohler.npm-intellisense \
+        --install-extension sujan.code-blue \
+        --install-extension waderyan.gitblame \
+        --install-extension ms-vscode.go \
+        --install-extension in4margaret.compareit \
+        --install-extension andys8.jest-snippets \
+        --install-extension euskadi31.json-pretty-printer \
+        --install-extension yatki.vscode-surround \
+        --install-extension wmaurer.change-case
 
 echo "Copying your SSH key to your clipboard"
 pbcopy < ~/.ssh/id_rsa.pub
