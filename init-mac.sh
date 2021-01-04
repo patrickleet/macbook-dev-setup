f65e4a4fcc2ae1a5290488ae44a64353cb443e2d
Patrick Lee Scott <pat@patscott.io>
2021-01-04 17:25:25 -0600

    Update init-mac.sh

--- init-mac.sh
+++ init-mac.sh
@@ -109,7 +109,9 @@ fi;
 if [ -x "$(command -v kubectl)" ]; then
     echo "kubectl installed"
 else
-    brew install kubernetes-cli
+    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl"
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
     echo "\nsource <(kubectl completion zsh)" >> ~/.zshrc
 fi;
 
